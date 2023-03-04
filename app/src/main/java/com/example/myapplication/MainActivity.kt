01c6e6392c1d9528984abbbaf9c19666f0d7b080
valentine <valentinetuyrin@gmail.com>
2023-03-05 00:08:02 +0300

    toolBar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+            R.id.sync -> {
+                Toast.makeText(this, "sync", Toast.LENGTH_SHORT).show()
+
+            }
+            R.id.delete -> {
+                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+        }
+        return true
     }
 }
